ed92615cdd137f9e0d60494ec3bf8f0552304bdb
CollinJAycock <collin.j.aycock@gmail.com>
2019-06-25 19:18:47 -0700

    AMI and Terraforms are working

--- vault-cluster-private/.terraform/modules/security_group_rules/modules/consul-cluster/main.tf
+++ vault-cluster-private/.terraform/modules/security_group_rules/modules/consul-cluster/main.tf
@@ -1,10 +1,9 @@
-# ----------------------------------------------------------------------------------------------------------------------
-# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
-# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
-# ----------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+# THESE TEMPLATES REQUIRE TERRAFORM VERSION 0.8 AND ABOVE
+# ---------------------------------------------------------------------------------------------------------------------
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.9.3"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -12,25 +11,22 @@ terraform {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_autoscaling_group" "autoscaling_group" {
-  name_prefix = var.cluster_name
+  name_prefix = "${var.cluster_name}"
 
-  launch_configuration = aws_launch_configuration.launch_configuration.name
+  launch_configuration = "${aws_launch_configuration.launch_configuration.name}"
 
-  availability_zones  = var.availability_zones
-  vpc_zone_identifier = var.subnet_ids
+  availability_zones  = "${var.availability_zones}"
+  vpc_zone_identifier = "${var.subnet_ids}"
 
   # Run a fixed number of instances in the ASG
-  min_size             = var.cluster_size
-  max_size             = var.cluster_size
-  desired_capacity     = var.cluster_size
-  termination_policies = [var.termination_policies]
+  min_size             = "${var.cluster_size}"
+  max_size             = "${var.cluster_size}"
+  desired_capacity     = "${var.cluster_size}"
+  termination_policies = ["${var.termination_policies}"]
 
-  health_check_type         = var.health_check_type
-  health_check_grace_period = var.health_check_grace_period
-  wait_for_capacity_timeout = var.wait_for_capacity_timeout
-  service_linked_role_arn   = var.service_linked_role_arn
-
-  enabled_metrics = var.enabled_metrics
+  health_check_type         = "${var.health_check_type}"
+  health_check_grace_period = "${var.health_check_grace_period}"
+  wait_for_capacity_timeout = "${var.wait_for_capacity_timeout}"
 
   tags = [
     {
@@ -53,30 +49,23 @@ resource "aws_autoscaling_group" "autoscaling_group" {
 
 resource "aws_launch_configuration" "launch_configuration" {
   name_prefix   = "${var.cluster_name}-"
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-  user_data     = var.user_data
-  spot_price    = var.spot_price
+  image_id      = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  user_data     = "${var.user_data}"
+  spot_price    = "${var.spot_price}"
 
-  iam_instance_profile = var.enable_iam_setup ? element(
-    concat(aws_iam_instance_profile.instance_profile.*.name, [""]),
-    0,
-  ) : var.iam_instance_profile_name
-  key_name = var.ssh_key_name
+  iam_instance_profile        = "${aws_iam_instance_profile.instance_profile.name}"
+  key_name                    = "${var.ssh_key_name}"
+  security_groups             = "${concat(list(aws_security_group.lc_security_group.id), var.additional_security_group_ids)}"
+  placement_tenancy           = "${var.tenancy}"
+  associate_public_ip_address = "${var.associate_public_ip_address}"
 
-  security_groups = concat(
-    aws_security_group.lc_security_group.id,
-    var.additional_security_group_ids,
-  )
-  placement_tenancy           = var.tenancy
-  associate_public_ip_address = var.associate_public_ip_address
-
-  ebs_optimized = var.root_volume_ebs_optimized
+  ebs_optimized = "${var.root_volume_ebs_optimized}"
 
   root_block_device {
-    volume_type           = var.root_volume_type
-    volume_size           = var.root_volume_size
-    delete_on_termination = var.root_volume_delete_on_termination
+    volume_type           = "${var.root_volume_type}"
+    volume_size           = "${var.root_volume_size}"
+    delete_on_termination = "${var.root_volume_delete_on_termination}"
   }
 
   # Important note: whenever using a launch configuration with an auto scaling group, you must set
@@ -96,9 +85,9 @@ resource "aws_launch_configuration" "launch_configuration" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_security_group" "lc_security_group" {
-  name_prefix = var.cluster_name
+  name_prefix = "${var.cluster_name}"
   description = "Security group for the ${var.cluster_name} launch configuration"
-  vpc_id      = var.vpc_id
+  vpc_id      = "${var.vpc_id}"
 
   # aws_launch_configuration.launch_configuration in this module sets create_before_destroy to true, which means
   # everything it depends on, including this resource, must set it as well, or you'll get cyclic dependency errors
@@ -107,34 +96,29 @@ resource "aws_security_group" "lc_security_group" {
     create_before_destroy = true
   }
 
-  tags = merge(
-    {
-      "Name" = var.cluster_name
-    },
-    var.security_group_tags,
-  )
+  tags = "${merge(map("Name", var.cluster_name), var.security_group_tags)}"
 }
 
 resource "aws_security_group_rule" "allow_ssh_inbound" {
-  count       = length(var.allowed_ssh_cidr_blocks) >= 1 ? 1 : 0
+  count       = "${length(var.allowed_ssh_cidr_blocks) >= 1 ? 1 : 0}"
   type        = "ingress"
-  from_port   = var.ssh_port
-  to_port     = var.ssh_port
+  from_port   = "${var.ssh_port}"
+  to_port     = "${var.ssh_port}"
   protocol    = "tcp"
-  cidr_blocks = var.allowed_ssh_cidr_blocks
+  cidr_blocks = "${var.allowed_ssh_cidr_blocks}"
 
-  security_group_id = aws_security_group.lc_security_group.id
+  security_group_id = "${aws_security_group.lc_security_group.id}"
 }
 
 resource "aws_security_group_rule" "allow_ssh_inbound_from_security_group_ids" {
-  count                    = var.allowed_ssh_security_group_count
+  count                    = "${length(var.allowed_ssh_security_group_ids)}"
   type                     = "ingress"
-  from_port                = var.ssh_port
-  to_port                  = var.ssh_port
+  from_port                = "${var.ssh_port}"
+  to_port                  = "${var.ssh_port}"
   protocol                 = "tcp"
-  source_security_group_id = element(var.allowed_ssh_security_group_ids, count.index)
+  source_security_group_id = "${element(var.allowed_ssh_security_group_ids, count.index)}"
 
-  security_group_id = aws_security_group.lc_security_group.id
+  security_group_id = "${aws_security_group.lc_security_group.id}"
 }
 
 resource "aws_security_group_rule" "allow_all_outbound" {
@@ -142,9 +126,9 @@ resource "aws_security_group_rule" "allow_all_outbound" {
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = "0.0.0.0/0"
 
-  security_group_id = aws_security_group.lc_security_group.id
+  security_group_id = "${aws_security_group.lc_security_group.id}"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -154,17 +138,17 @@ resource "aws_security_group_rule" "allow_all_outbound" {
 module "security_group_rules" {
   source = "../consul-security-group-rules"
 
-  security_group_id                    = aws_security_group.lc_security_group.id
-  allowed_inbound_cidr_blocks          = var.allowed_inbound_cidr_blocks
-  allowed_inbound_security_group_ids   = var.allowed_inbound_security_group_ids
-  allowed_inbound_security_group_count = var.allowed_inbound_security_group_count
+  security_group_id                    = "${aws_security_group.lc_security_group.id}"
+  allowed_inbound_cidr_blocks          = "${var.allowed_inbound_cidr_blocks}"
+  allowed_inbound_security_group_ids   = "${var.allowed_inbound_security_group_ids}"
+  allowed_inbound_security_group_count = "${var.allowed_inbound_security_group_count}"
 
-  server_rpc_port = var.server_rpc_port
-  cli_rpc_port    = var.cli_rpc_port
-  serf_lan_port   = var.serf_lan_port
-  serf_wan_port   = var.serf_wan_port
-  http_api_port   = var.http_api_port
-  dns_port        = var.dns_port
+  server_rpc_port = "${var.server_rpc_port}"
+  cli_rpc_port    = "${var.cli_rpc_port}"
+  serf_lan_port   = "${var.serf_lan_port}"
+  serf_wan_port   = "${var.serf_wan_port}"
+  http_api_port   = "${var.http_api_port}"
+  dns_port        = "${var.dns_port}"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -174,11 +158,9 @@ module "security_group_rules" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_iam_instance_profile" "instance_profile" {
-  count = var.enable_iam_setup ? 1 : 0
-
-  name_prefix = var.cluster_name
-  path        = var.instance_profile_path
-  role        = element(concat(aws_iam_role.instance_role.*.name, [""]), 0)
+  name_prefix = "${var.cluster_name}"
+  path        = "${var.instance_profile_path}"
+  role        = "${aws_iam_role.instance_role.name}"
 
   # aws_launch_configuration.launch_configuration in this module sets create_before_destroy to true, which means
   # everything it depends on, including this resource, must set it as well, or you'll get cyclic dependency errors
@@ -189,10 +171,8 @@ resource "aws_iam_instance_profile" "instance_profile" {
 }
 
 resource "aws_iam_role" "instance_role" {
-  count = var.enable_iam_setup ? 1 : 0
-
-  name_prefix        = var.cluster_name
-  assume_role_policy = data.aws_iam_policy_document.instance_role.json
+  name_prefix        = "${var.cluster_name}"
+  assume_role_policy = "${data.aws_iam_policy_document.instance_role.json}"
 
   # aws_iam_instance_profile.instance_profile in this module sets create_before_destroy to true, which means
   # everything it depends on, including this resource, must set it as well, or you'll get cyclic dependency errors
@@ -221,7 +201,5 @@ data "aws_iam_policy_document" "instance_role" {
 module "iam_policies" {
   source = "../consul-iam-policies"
 
-  enabled     = var.enable_iam_setup
-  iam_role_id = element(concat(aws_iam_role.instance_role.*.id, [""]), 0)
+  iam_role_id = "${aws_iam_role.instance_role.id}"
 }
-
